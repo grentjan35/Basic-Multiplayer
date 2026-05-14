352c0232ff000b950f095d6580448affc239d843
grentjan35 <grentjan35@gmail.com>
2026-05-13 18:22:03 -0700

    ducky

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ const WORLD_WIDTH = 5000;
 const WORLD_HEIGHT = 2000;
 
 // Available character spritesheets
-const CHARACTERS = ['Bookie', 'Getaway Driver', 'Informant', 'Safecracker', 'smuggler', 'Street Thug', 'Boss'];
+const CHARACTERS = ['Bookie', 'Getaway Driver', 'Informant', 'Safecracker', 'smuggler', 'Street Thug', 'Boss', 'Distractor Duck'];
 
 // Game state - SERVER ONLY
 const players = new Map();
